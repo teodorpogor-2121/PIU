ddeff5bb90ffa14883da5225053733ed3a33a782
teodorpogor-2121 <teodor.pogor@student.usv.ro>
2026-03-13 00:48:35 +0200

    primul commit

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Parfumuri;
+
+namespace Parfumuri
+{
+    public class Parfum
+    {
+        
+        
+    public string Id { get; set; }
+    public string Nume { get; set; }
+    public string Brand { get; set; }
+    public Concentratie Concentratie { get; set; }
+    public int CantitateMl { get; set; }
+    public int Stoc { get; set; }
+    public decimal Pret { get; set; }
+
+    public Parfum(string id, string nume, string brand, Concentratie concentratie, int cantitateMl, int stoc, decimal pret)
+    {
+        Id = id;
+        Nume = nume;
+        Brand = brand;
+        Concentratie = concentratie;
+        CantitateMl = cantitateMl;
+        Stoc = stoc;
+        Pret = pret;
+    }
+}
+}
---
+++ Class3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Parfumuri;
+
+namespace Parfumuri
+{
+    public class Tranzactie
+    {
+        public string Id { get; set; }
+        public string ParfumId { get; set; }
+        public string ClientId { get; set; }
+        public DateTime Data { get; set; }
+        public string Tip { get; set; } // vanzare sau retur adica tipul tranzactiei
+        public decimal Valoare { get; set; }
+    }
+}
